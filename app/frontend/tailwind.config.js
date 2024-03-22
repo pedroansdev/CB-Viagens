05b15fda0699e8dd60099a140bd75dbc6855feb8
Pedro Augusto <pedroaugdev@gmail.com>
2024-03-22 15:52:18 -0300

    Limpando estilos antigos e adicionando o tailwind

---
+++ app/frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,vue}"
+  ],
+  theme: {
+    colors: {
+      'cb-gray': '#2A3041',
+      'cb-light-gray': '#F3F3F3',
+      'cb-blue': '#03A8B5',
+      'cb-yellow': '#F3A531'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
